d3aeb04a4c2b8712f89191b5f399efd3efdb829e
jagam <jagam@LAPTOP-R3HMSK5P>
2017-10-23 02:32:25 +0530

    search by name & id service

--- src/com/cert/util/QueryDatabase.java
+++ src/com/cert/util/QueryDatabase.java
@@ -2,18 +2,26 @@ package com.cert.util;
 
 import org.bson.Document;
 
-import com.mongodb.client.FindIterable;
-import com.mongodb.util.JSON;
-import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+import com.mongodb.client.MongoCursor;
 
 public class QueryDatabase {
 	
-	public FindIterable<Document> searchCetificatesbyEmpId(String empId) {
+	public MongoCursor<Document> searchCetificatesbyEmpId(String empId) {
 		
 		DBConnection db = new DBConnection("DB_CERTIFICATION_DETAILS");
 		Document query = new Document("EmployeeId", empId);
 		
-		FindIterable<Document> response = db.runQuery(query);
+		MongoCursor<Document> response = db.runQuery(query);
+		db.closeDBConnection();
+		return response;
+	}
+	
+	public MongoCursor<Document> searchCetificatesbyEmpName(String empName) {
+		
+		DBConnection db = new DBConnection("DB_CERTIFICATION_DETAILS");
+		Document query = new Document("EmployeeName", empName);
+		
+		MongoCursor<Document> response = db.runQuery(query);
 		db.closeDBConnection();
 		return response;
 	}
